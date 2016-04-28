d96e3118a89bf647854d75ac7c29c74d4829cf84
bsergeytpp <bsergeytpp@gmail.com>
2016-04-28 12:26:48 +0300

    Remove content files; some work on tinyMCE editor in admin panel

--- admin/add_news.php
+++ admin/add_news.php
@@ -7,6 +7,12 @@
     <head>
         <meta charset="utf-8">
         <title>Форма добавления новости</title>
+		<script src="../scripts/tinymce/tinymce.min.js"></script>
+		<script type="text/javascript">
+		  tinymce.init({
+			selector: '#mytextarea'
+		  });
+		</script>
     </head>
     <body>
         <div id="background-div"></div>
@@ -14,7 +20,7 @@
         <form action="save_news.php" method="post">
 			<p>Дата: <input type="date" name="news-date" size="10" required></p>
 			<p>Заголовок: <input type="text" name="news-header" size="20" required></p>
-			<p>Текст: <textarea name="news-text" size="50" required></textarea></p>
+			<p>Текст: <textarea id="mytextarea" name="news-text" size="50"></textarea></p>
 			<!--<p>Есть логотип: <input name="hasImage" type="checkbox" required>Да/Нет</input></p>-->
 			<p><input type="submit" value="Добавить"></p>
 		</form>

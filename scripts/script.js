bb82f0e6a9a76d85f6df348f493d8e47748906a0
bsergeytpp <bsergeytpp@gmail.com>
2016-05-31 15:01:38 +0300

    JS: move admin functions to Admin class

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 /*Passive event listeners for Blink*/
-// Dunno if it works.
 var supportsPassive = false;
 
 try {
@@ -32,7 +31,230 @@ addEventListenerWithOptions(document, "wheel", function(e) {
 
 /***********************/
 
-addEventListenerWithOptions(document, 'onscroll', function(e) {
+function Admin() {
+	this._isAdmin = false;
+	this._XMLHttpRequest = null;
+	this._editBtns = [];
+	this._responseObject;
+	this._editDiv = null;
+};
+
+Admin.prototype.getIsAdmin = function() {
+	return this._isAdmin;
+};
+
+Admin.prototype.checkIfAdmin = function() {
+	var self = this;
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function () {
+		if(request.readyState == 4) {
+			clearTimeout(timeout);
+			var resp = request.getResponseHeader('IsAdmin');
+			
+			if(resp !== null) {
+				console.log("Вы - Админ. Поздравляю!");
+				self._isAdmin = true;
+				appendScript('scripts/tinymce/tinymce.min.js');
+				self.addEditBtn();
+			}
+			else {
+				console.log("Вы - не Админ. Херово!");
+			}
+		}
+	};
+	var timeout = setTimeout(function() {
+		request.abort();
+	}, 60*1000);
+	request.open('HEAD', 'content/json.php', true);
+	request.send();
+};
+
+Admin.prototype.addEditBtn = function() {
+	var pattern = '';
+	if(document.getElementsByClassName('article-news').length > 0 ||
+	   document.getElementsByClassName('news-full-container').length > 0) {
+		pattern = 'news';
+	}
+	else if(document.getElementsByClassName('article-publs').length > 0 ||
+	        document.getElementsByClassName('publs-full-container').length > 0) {
+		pattern = 'publs';
+	}
+	else return;
+	
+	var elem = document.getElementsByClassName('article-'+pattern);
+	
+	// если элемент открыт
+	if(elem.length === 0) {
+		elem = document.getElementsByClassName(pattern+'-full-container');
+	}
+
+	for(var i=0, len=elem.length; i<len; i++) {
+		this._editBtns[i] = document.createElement('div');
+		this._editBtns[i].className = 'admin-edit-button';
+		var firstChild = elem[i].children[0];
+		elem[i].insertBefore(this._editBtns[i], firstChild);
+	}
+
+	this.initAdminEdit();
+};
+
+Admin.prototype.initAdminEdit = function() {
+	var self = this;
+	for(var i=0, len=this._editBtns.length; i<len; i++) {
+		(function() {
+			self._editBtns[i].addEventListener('click', self.addHandlerOnEditBtns.bind(self), false);
+		})();
+	}
+};
+
+Admin.prototype.addHandlerOnEditBtns = function(e) {
+	var id = e.target.parentNode.getAttribute('id');
+	var className = e.target.parentNode.className;
+	var self = this;
+	
+	this._getElemByDBId(className, id, function() {
+		var response = this.responseText;
+		
+		if(typeof response === 'string') {
+			self._responseObject = JSON.parse(response);
+		}
+		
+		if(typeof self._responseObject === 'object') {
+			if(document.getElementsByClassName('admin-edit-elem')[0] !== undefined) {
+				self._editDiv = null;
+				document.body.removeChild(document.getElementsByClassName('admin-edit-elem')[0]);
+			}
+	
+			// создаем все необходимые элементы
+			self._createEditDiv(className);
+			
+			// делаем из textarea объект tinymce
+			initTinyMCE('.admin-edit-elem textarea', false);
+			
+			// вешаем на кнопки события
+			self._editDiv.addEventListener('click', function(e) {
+				var target = e.target;
+				e.preventDefault();
+				
+				if(target.innerHTML === 'Отменить') {
+					e.stopPropagation();
+					document.body.removeChild(this);	// удаляем div редактирования
+				}
+				else if(target.innerHTML === 'Сохранить') {
+					e.stopPropagation();
+					var updatedText = tinymce.activeEditor.getContent();
+					// запрос на сохранение элемента
+					var reqTarget = (this.className === 'article-news') ? 'news' : 'publs';
+					this._sendSaveRequest({
+						'id': id,
+						'text': updatedText
+					   },
+					   'POST', 
+					   'admin/update_'+reqTarget+'.php', 
+					   'application/x-www-form-urlencoded');
+					document.body.removeChild(this);	// удаляем div редактирования
+				} 
+				
+			}, false);
+		}
+		else console.log(response);
+	});
+};
+
+Admin.prototype._createEditDiv = function(className) {
+	var div = document.createElement('div');
+	var form = document.createElement('form');
+	var textarea = document.createElement('textarea');
+	var saveBtn = document.createElement('a');
+	var closeBtn = document.createElement('a');
+	saveBtn.innerHTML = 'Сохранить';
+	saveBtn.setAttribute('href', '#');
+	closeBtn.innerHTML = 'Отменить';
+	closeBtn.setAttribute('href', '#');
+	div.className = 'admin-edit-elem'; 
+	
+	if(className === 'article-news') {
+		form.innerHTML = 'ID: ' + this._responseObject['news_id'] + ' | ' + 
+						 'Загловок: ' + this._responseObject['news_header'];
+		textarea.innerHTML = this._responseObject['news_text'];
+	}
+	else {
+		form.innerHTML = 'ID: ' + this._responseObject['publs_id'] + ' | ' + 
+						 'Загловок: ' + this._responseObject['publs_header'];
+		textarea.innerHTML = this._responseObject['publs_text'];
+	} 
+		
+	form.appendChild(textarea);
+	form.appendChild(saveBtn);
+	form.appendChild(closeBtn);
+	div.appendChild(form);
+	document.body.appendChild(div);
+	
+	this._editDiv = div;
+};
+
+Admin.prototype._sendSaveRequest = function(argArr, reqType, reqTarget, contentType) {
+	var data = '', j = 1;
+	var self = this;
+	
+	for(var key in argArr) {
+		var val = argArr[key];
+		data += key + '=' + val;
+		(j++ < Object.keys(argArr).length) ? data += '&' : console.log('I dunno');
+	}
+	
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if(request.readyState == 4) {
+			clearTimeout(timeout);
+			if(request.status != 200) {
+				console.log('Ошибка: ' + request.responseText);
+			}
+			else console.log('Запрос отправлен. Все - хорошо.');
+		}
+	};
+	var timeout = setTimeout(function() {
+		request.abort();
+	}, 60*1000);
+	request.open(reqType, reqTarget, true);
+	request.setRequestHeader("Content-Type", contentType);
+	request.send(data);
+}
+
+Admin.prototype._getElemByDBId = function(className, id, callback) {
+	var request = new XMLHttpRequest();
+	var pattern = (className === 'article-news') ? 'news' : 'publs' ;
+	var self = this;
+	
+	request.onreadystatechange = function () {
+		if(request.readyState == 4) {
+			clearTimeout(timeout); 
+			
+			if(request.status != 200) {
+				console.log('wha?');
+			}
+			else {
+				var resp = request.responseText;
+				if(resp != null) {
+					if(typeof callback  == 'function') {
+						callback.call(request);
+					}
+				}
+				else {
+					console.log("Херово!");
+				}	
+			}
+		}
+	};
+	
+	var timeout = setTimeout(function() {
+		request.abort();
+	}, 60*1000);
+	request.open('GET', 'admin/get_'+pattern+'_by_id.php?id=' + id, true);
+	request.send();
+};
+
+addEventListenerWithOptions(document, 'scroll', function(e) {
     var article = document.getElementsByClassName('article')[0];
     var scrollBtn = document.getElementsByClassName('scroll-button')[0];
     var header = document.getElementsByClassName('header')[0];
@@ -61,9 +283,11 @@ addEventListenerWithOptions(document, 'DOMContentLoaded', function() {
 }, {passive: true});
 
 addEventListenerWithOptions(document, 'DOMContentLoaded', function(e) { 
-	checkIfAdmin(appendScript.bind(null, 'scripts/tinymce/tinymce.min.js'));
-	checkIfAdmin(addEditBtn.bind(null,'news')); 
-	checkIfAdmin(addEditBtn.bind(null, 'publs'));
+	var admin = new Admin();
+	admin.checkIfAdmin();
+	//admin.checkIfAdmin(appendScript.bind(null, 'scripts/tinymce/tinymce.min.js'));
+	//admin.checkIfAdmin(admin.addEditBtn.bind(admin,'news'));
+	//admin.checkIfAdmin(admin.addEditBtn.bind(admin,'publs'));
 }, {passive: true});
 
 function initTinyMCE(className, isInline) {
@@ -76,181 +300,6 @@ function initTinyMCE(className, isInline) {
 		width: 400,
 		height: 170
 	});
-}
-
-function addEditBtn(pattern) {
-	var elem = document.getElementsByClassName('article-'+pattern);
-	
-	// если элемент открыт
-	if(elem.length === 0) {
-		elem = document.getElementsByClassName(pattern+'-full-container');
-	}
-	
-	for(var i=0, len=elem.length; i<len; i++) {
-		var div = document.createElement('div');
-		div.className = 'admin-edit-button';
-		var firstChild = elem[i].children[0];
-		elem[i].insertBefore(div, firstChild);
-	}
-	
-	initAdminEdit();
-}
-
-function initAdminEdit() {
-	var editBtns = document.getElementsByClassName('admin-edit-button');
-	
-	for(var i=0, len=editBtns.length; i<len; i++) {
-		(function() {
-			editBtns[i].addEventListener('click', addHandlerOnEditBtns, false);
-		})();
-	}
-}
-
-function addHandlerOnEditBtns(e) {
-	var id = this.parentNode.getAttribute('id');
-	var className = this.parentNode.className;
-	getElemById(className, id, function() {
-		var response = this.responseText;
-		
-		if(typeof response === 'string') {
-			var obj = JSON.parse(response);
-		}
-		
-		if(typeof obj === 'object') {
-			if(checkPrevEditDivs()) {
-				document.body.removeChild(document.getElementsByClassName('admin-edit-elem')[0]);
-			}
-			
-			// создаем все необходимые элементы
-			var editDiv = createEditDiv(obj, className);
-			
-			// делаем из textarea объект tinymce
-			initTinyMCE('.admin-edit-elem textarea', false);
-			
-			// вешаем на кнопки события
-			editDiv.addEventListener('click', function(e) {
-				var target = e.target;
-				e.preventDefault();
-				
-				if(target.innerHTML === 'Отменить') {
-					e.stopPropagation();
-					document.body.removeChild(this);	// удаляем div редактирования
-				}
-				else if(target.innerHTML === 'Сохранить') {
-					e.stopPropagation();
-					var updatedText = tinymce.activeEditor.getContent();
-					// запрос на сохранение элемента
-					var reqTarget = (this.className === 'article-news') ? 'news' : 'publs';
-					sendSaveRequest({
-						'id': id,
-						'text': updatedText
-					   },
-					   'POST', 
-					   'admin/update_'+reqTarget+'.php', 
-					   'application/x-www-form-urlencoded');
-					document.body.removeChild(this);	// удаляем div редактирования
-				} 
-				
-			}, false);
-		}
-		else console.log(response);
-	});
-}
-
-function createEditDiv(obj, className) {
-	var div = document.createElement('div');
-	var form = document.createElement('form');
-	var textarea = document.createElement('textarea');
-	var saveBtn = document.createElement('a');
-	var closeBtn = document.createElement('a');
-	saveBtn.innerHTML = 'Сохранить';
-	saveBtn.setAttribute('href', '#');
-	closeBtn.innerHTML = 'Отменить';
-	closeBtn.setAttribute('href', '#');
-	div.className = 'admin-edit-elem'; 
-	
-	if(className === 'article-news') {
-		form.innerHTML = 'ID: ' + obj['news_id'] + ' | ' + 
-						 'Загловок: ' + obj['news_header'];
-		textarea.innerHTML = obj['news_text'];
-	}
-	else {
-		form.innerHTML = 'ID: ' + obj['publs_id'] + ' | ' + 
-						 'Загловок: ' + obj['publs_header'];
-		textarea.innerHTML = obj['publs_text'];
-	} 
-		
-	form.appendChild(textarea);
-	form.appendChild(saveBtn);
-	form.appendChild(closeBtn);
-	div.appendChild(form);
-	document.body.appendChild(div);
-	
-	return div;
-}
-
-function checkPrevEditDivs() {
-	var div = document.getElementsByClassName('admin-edit-elem');
-	
-	return (div.length > 0) ? true : false;
-}
-
-function sendSaveRequest(argArr, reqType, reqTarget, contentType) {
-	var data = '', j = 1;
-	
-	for(var key in argArr) {
-		var val = argArr[key];
-		data += key + '=' + val;
-		(j++ < Object.keys(argArr).length) ? data += '&' : console.log('I dunno');
-	}
-	
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if(request.readyState == 4) {
-			clearTimeout(timeout);
-			if(request.status != 200) {
-				console.log('Ошибка: ' + request.responseText);
-			}
-			else console.log('Запрос отправлен. Все - хорошо.');
-		}
-	};
-	var timeout = setTimeout(function() {
-		request.abort();
-	}, 60*1000);
-	request.open(reqType, reqTarget, true);
-	request.setRequestHeader("Content-Type", contentType);
-	request.send(data);
-}
-
-function getElemById(className, id, callback) {
-	var request = new XMLHttpRequest();
-	var pattern = (className === 'article-news') ? 'news' : 'publs' ;
-	request.onreadystatechange = function () {
-		if(request.readyState == 4) {
-			clearTimeout(timeout); 
-			
-			if(request.status != 200) {
-				console.log('wha?');
-			}
-			else {
-				var resp = request.responseText;
-				if(resp != null) {
-					if(typeof callback  == 'function') {
-						callback.call(request);
-					}
-				}
-				else {
-					console.log("Херово!");
-				}	
-			}
-		}
-	};
-	
-	var timeout = setTimeout(function() {
-		request.abort();
-	}, 60*1000);
-	request.open('GET', 'admin/get_'+pattern+'_by_id.php?id=' + id, true);
-	request.send();
 }
 
 function navigateUlList(e) {
@@ -269,7 +318,7 @@ function navigateUlList(e) {
 		urlParams[pair[0]] = pair[1];
 	}
 	
-	var pageNum = getParam('page', urlParams);
+	var pageNum = getUrlParam('page', urlParams);
 	
 	if(!pageNum) pageNum = 1;
 				
@@ -302,7 +351,7 @@ function navigateUlList(e) {
 	}		
 }
 
-function getParam(value, obj) {
+function getUrlParam(value, obj) {
 	for(var param in obj) {
 		if(param == value) {
 			return obj[param];

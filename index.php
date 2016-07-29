445b41dbfa3b628ea48e3c20d20316db1c7dc6d7
bsergeytpp <bsergeytpp@gmail.com>
2016-07-29 10:48:51 +0300

    Small fix for markup

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width; initial-scale=1.0;">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Главная страница</title>
         <link type="text/css" rel="StyleSheet" href="styles/styles.css" />
 		<link rel="shortcut icon" href="favicon.png" type="image/png">

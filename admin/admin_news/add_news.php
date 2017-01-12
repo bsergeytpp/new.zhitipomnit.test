c29c2169aef498af49d47ff14bf6bd0a843e4e5d
bsergeytpp <bsergeytpp@gmail.com>
2017-01-12 10:53:47 +0300

    News: initial support for adding images to news from admin panel

--- admin/admin_news/add_news.php
+++ admin/admin_news/add_news.php
@@ -22,11 +22,17 @@
     <body>
         <div id="background-div"></div>
 		<h2>Форма добавления новости:</h2>
-        <form action="save_news.php" method="post">
+        <form action="save_news.php" method="post" enctype="multipart/form-data">
 			<p>Дата: <input type="date" name="news-date" size="10" required></p>
 			<p>Заголовок: <input type="text" name="news-header" size="20" required></p>
 			<p>Текст: <textarea id="news-textarea" name="news-text" size="50"></textarea></p>
 			<!--<p>Есть логотип: <input name="hasImage" type="checkbox" required>Да/Нет</input></p>-->
+			<p>Добавить картинку: <input type="file" name="news-image"></p>
+			<p>
+				<input type="radio" name="news-image-align" value="left">Слева
+				<input type="radio" name="news-image-align" value="center">По центру
+				<input type="radio" name="news-image-align" value="right">Справа
+			</p>
 			<p><input type="submit" value="Добавить"></p>
 		</form>
     </body>
--- admin/admin_news/save_news.php
+++ admin/admin_news/save_news.php
@@ -7,9 +7,28 @@
 	
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if($link) {
+			$tmpName = $_FILES['news-image']['tmp_name'];
+			$img = null;
+			$imgAlign = 'center';
+			
+			if(is_uploaded_file($tmpName)) {
+				//if(!file_exists($_FILES['news-image']['name'])) {
+					move_uploaded_file($tmpName, 'admin/images/'.$_FILES['news-image']['name']);
+					$img = 'admin/images/'.$_FILES['news-image']['name'];
+					$imgAlign = $_POST['news-image-align'];
+				//}
+			}
+			
 			$date = $_POST['news-date'];
 			$header = clearStr($_POST['news-header']);
 			$text = clearStr($_POST['news-text']);
+			
+			if($img !== null) {
+				$tmpDiv = "<div style='text-align: $imgAlign'><img src='$img' alt=''></div>";
+				$text = pg_escape_string(str_replace('$IMAGE1', $tmpDiv, $text));
+				echo "новый текст: ".$text;
+			}
+			
 			$author = (isset($_SESSION['user'])) ? $_SESSION['user'] : 'default';
 			$query = "INSERT INTO news (news_date, news_header, news_text, news_author)
 					  VALUES ('$date', '$header', '$text', '" . $author . "')";
